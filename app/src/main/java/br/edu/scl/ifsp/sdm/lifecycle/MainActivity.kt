c8b594cd84045b6f6250ce2280ef9dd70f29d29f
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2025-09-06 15:20:04 -0300

    feat: implementing onSaveInstanceState

--- app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/MainActivity.kt
@@ -3,7 +3,10 @@ package br.edu.scl.ifsp.sdm.lifecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.widget.EditText
+import androidx.core.view.children
 import androidx.core.widget.doAfterTextChanged
 import br.edu.scl.ifsp.sdm.lifecycle.databinding.ActivityMainBinding
 import br.edu.scl.ifsp.sdm.lifecycle.databinding.TilePhoneBinding
@@ -14,6 +17,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var filledChars: Int = 0
+
+    companion object {
+        const val FILLED_CHARS = "FILLED_CHARS"
+        const val PHONES = "PHONES"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,31 +42,68 @@ class MainActivity : AppCompatActivity() {
             }
         }
         supportActionBar?.subtitle = getString(R.string.main)
-        Log.v(getString(R.string.app_name), "Main - onCreate(): inicio COMPLETO")
+        Log.v(getString(R.string.app_name), "Main - onCreate(): início COMPLETO")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(FILLED_CHARS, filledChars)
+
+        val phones = mutableListOf<String>()
+        activityMainBinding.phonesLl.children.forEachIndexed { index, view ->
+            if(index != 0){
+                (view as EditText).text.toString().let {
+                    phones.add(it)
+                }
+            }
+        }
+        outState.putStringArray(PHONES, phones.toTypedArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getInt(FILLED_CHARS, 0).let {
+            filledChars = it
+            "${getString(R.string.filled_chars)} ${filledChars}".also{fc ->
+                activityMainBinding.filledCharsTv.text = fc
+            }
+        }
+
+        savedInstanceState.getStringArray(PHONES)?.forEach {phone ->
+            TilePhoneBinding.inflate(layoutInflater).apply {
+                root.setText(phone)
+                activityMainBinding.phonesLl.addView(root)
+            }
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.v(getString(R.string.app_name), "Main - onsTART(): inicio VISIVEL")
+        Log.v(getString(R.string.app_name), "Main - onStart(): início VISÍVEL")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.v(getString(R.string.app_name), "Main - onResume(): início PRIMEIRO PLANO")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.v(getString(R.string.app_name), "Main - onPause(): fim PRIMEIRO PLANO")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.v(getString(R.string.app_name), "Main - onStop(): fim VISÍVEL")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.v(getString(R.string.app_name), "Main - onDestroy(): fim COMPLETO")
     }
 
     override fun onRestart() {
         super.onRestart()
+        Log.v(getString(R.string.app_name), "Main - onRestart(): preparando onStart()")
     }
 }
